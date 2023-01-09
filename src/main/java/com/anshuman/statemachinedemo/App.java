febdd6074b3802a21d1b8a6bb3503b8b07b3b4ca
Anshuman Sanghvi <anshuman.sanghvi@gmail.com>
2023-01-09 16:46:14 +0530

    Data source configuration

--- src/main/java/com/anshuman/statemachinedemo/App.java
+++ src/main/java/com/anshuman/statemachinedemo/App.java
@@ -2,12 +2,8 @@ package com.anshuman.statemachinedemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EntityScan(basePackages = {"com.anshuman.statemachinedemo.model.entity"})
-@EnableJpaRepositories(basePackages = {"com.anshuman.statemachinedemo.model.repository"})
 public class App {
 
     public static void main(String[] args) {
