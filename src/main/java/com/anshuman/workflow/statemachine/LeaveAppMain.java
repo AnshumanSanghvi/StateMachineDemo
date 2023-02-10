e9cf5387d9407cc731ed8ec3285c8da0e256c840
Anshuman Sanghvi <anshuman.sanghvi@gmail.com>
2023-02-10 16:18:18 +0530

    create leave app state machine factory.

--- src/main/java/com/anshuman/workflow/statemachine/LeaveAppMain.java
+++ src/main/java/com/anshuman/workflow/statemachine/LeaveAppMain.java
@@ -5,24 +5,29 @@ import static com.anshuman.workflow.statemachine.event.LeaveAppEvent.E_FORWARD;
 import static com.anshuman.workflow.statemachine.event.LeaveAppEvent.E_INITIALIZE;
 import static com.anshuman.workflow.statemachine.event.LeaveAppEvent.E_SUBMIT;
 import static com.anshuman.workflow.statemachine.event.LeaveAppEvent.E_TRIGGER_COMPLETE;
-import static com.anshuman.workflow.statemachine.event.LeaveAppEvent.E_TRIGGER_FLOW_JUNCTION;
 import static com.anshuman.workflow.statemachine.event.LeaveAppEvent.E_TRIGGER_REVIEW_OF;
+import static com.anshuman.workflow.statemachine.util.EventResultHelper.toResultDTOList;
+import static com.anshuman.workflow.statemachine.util.EventSendHelper.sendEvent;
+import static com.anshuman.workflow.statemachine.util.EventSendHelper.sendForwardEvent;
 
+import com.anshuman.workflow.data.model.entity.ContextEntity;
+import com.anshuman.workflow.statemachine.action.LeaveAppActions;
 import com.anshuman.workflow.statemachine.builder.LeaveAppSMBuilder;
+import com.anshuman.workflow.statemachine.data.dto.EventResultDTO;
 import com.anshuman.workflow.statemachine.event.LeaveAppEvent;
 import com.anshuman.workflow.statemachine.exception.StateMachineException;
+import com.anshuman.workflow.statemachine.persist.DefaultStateMachineAdapter;
 import com.anshuman.workflow.statemachine.state.LeaveAppState;
-import com.anshuman.workflow.statemachine.util.EventResultHelper;
-import com.anshuman.workflow.statemachine.util.EventSendHelper;
+import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.boot.CommandLineRunner;
-import org.springframework.context.ApplicationContext;
 import org.springframework.statemachine.StateMachine;
 import org.springframework.stereotype.Component;
 
@@ -31,11 +36,12 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class LeaveAppMain implements CommandLineRunner {
 
-    private final ApplicationContext applicationContext;
+    private final DefaultStateMachineAdapter<LeaveAppState, LeaveAppEvent, ContextEntity<LeaveAppState, LeaveAppEvent>> stateMachineAdapter;
 
     @Override
     public void run(String... args) throws Exception {
-        BeanFactory beanFactory = applicationContext.getAutowireCapableBeanFactory();
+        StateMachine<LeaveAppState, LeaveAppEvent> sm = stateMachineAdapter.create(LEAVE_APP_WF_V1);
+
         int reviewersCount = 3;
         Map<Integer, Long> reviewerMap = new LinkedHashMap<>(reviewersCount);
         reviewerMap.put(1, 234L);
@@ -44,18 +50,23 @@ public class LeaveAppMain implements CommandLineRunner {
         boolean isParallel = false;
         int maxChangeRequests = 3;
         int maxRollBackCount = 3;
+        LeaveAppActions.StateActions.initial(sm, reviewersCount, reviewerMap, isParallel,
+            maxChangeRequests, maxRollBackCount);
 
-        StateMachine<LeaveAppState, LeaveAppEvent> sm = createStateMachine(beanFactory, reviewerMap, isParallel, maxChangeRequests, maxRollBackCount);
+        List<EventResultDTO<LeaveAppState, LeaveAppEvent>> resultList = new ArrayList<>();
+        resultList.addAll(toResultDTOList(sendEvent(sm, E_INITIALIZE)));
+        resultList.addAll(toResultDTOList(sendEvent(sm, E_SUBMIT)));
+        resultList.addAll(toResultDTOList(sendEvent(sm, E_TRIGGER_REVIEW_OF)));
+        resultList.addAll(toResultDTOList(sendForwardEvent(sm, E_FORWARD, 1, 234L)));
+        resultList.addAll(toResultDTOList(sendForwardEvent(sm, E_FORWARD, 2, 123L)));
+        resultList.addAll(toResultDTOList(sendForwardEvent(sm, E_FORWARD, 3, 235L)));
+        resultList.addAll(toResultDTOList(sendEvent(sm, E_TRIGGER_COMPLETE)));
 
-        log.info("result: {}", EventResultHelper.toResultDTOString(EventSendHelper.sendEvents(sm, E_INITIALIZE, E_SUBMIT, E_TRIGGER_REVIEW_OF,
-            E_TRIGGER_FLOW_JUNCTION)));
-        log.info("result: {}", EventResultHelper.toResultDTOString(EventSendHelper.sendForwardEvent(sm, E_FORWARD, 1, 234L)));
-        log.info("result: {}", EventResultHelper.toResultDTOString(EventSendHelper.sendForwardEvent(sm, E_FORWARD, 2, 123L)));
-        log.info("result: {}", EventResultHelper.toResultDTOString(EventSendHelper.sendForwardEvent(sm, E_FORWARD, 3, 235L)));
-        log.info("result: {}", EventResultHelper.toResultDTOString(EventSendHelper.sendEvent(sm, E_TRIGGER_COMPLETE)));
+        log.info("SM Event Results: \n{}", resultList.stream()
+            .map(EventResultDTO::toString)
+            .collect(Collectors.joining("\n")));
 
-        log.info("Final SM State: {}, ExtendedState: [{}]",
-            sm.getState().getId(),
+        log.info("Final SM State: {}, ExtendedState: [{}]", sm.getState().getId(),
             sm.getExtendedState().getVariables().entrySet()
                 .stream()
                 .sorted(Comparator.comparing(entry -> (String) entry.getKey()))
@@ -67,8 +78,9 @@ public class LeaveAppMain implements CommandLineRunner {
         int maxRollBackCount) {
         try {
             int reviewerCount = reviewerMap.size();
-            StateMachine<LeaveAppState, LeaveAppEvent> sm = LeaveAppSMBuilder.createStateMachine(LEAVE_APP_WF_V1,
-                beanFactory, reviewerCount, reviewerMap, isParallel, maxChangeRequests, maxRollBackCount);
+            StateMachine<LeaveAppState, LeaveAppEvent> sm = LeaveAppSMBuilder
+                .createStateMachine(LEAVE_APP_WF_V1, beanFactory, reviewerCount, reviewerMap,
+                    isParallel, maxChangeRequests, maxRollBackCount);
             sm.startReactively().block();
             log.info("starting statemachine: {}", sm.getId());
             return sm;
---
+++ src/main/java/com/anshuman/workflow/statemachine/config/LeaveAppSMConfig.java
@@ -0,0 +1,163 @@
+package com.anshuman.workflow.statemachine.config;
+
+import static com.anshuman.workflow.statemachine.data.constant.LeaveAppSMConstants.*;
+import static com.anshuman.workflow.statemachine.event.LeaveAppEvent.*;
+import static com.anshuman.workflow.statemachine.state.LeaveAppState.S_CLOSED;
+import static com.anshuman.workflow.statemachine.state.LeaveAppState.S_COMPLETED;
+import static com.anshuman.workflow.statemachine.state.LeaveAppState.S_CREATED;
+import static com.anshuman.workflow.statemachine.state.LeaveAppState.S_INITIAL;
+import static com.anshuman.workflow.statemachine.state.LeaveAppState.S_SUBMITTED;
+import static com.anshuman.workflow.statemachine.state.LeaveAppState.S_UNDER_PROCESS;
+
+import com.anshuman.workflow.statemachine.action.LeaveAppActions.TransitionActions;
+import com.anshuman.workflow.statemachine.event.LeaveAppEvent;
+import com.anshuman.workflow.statemachine.guard.LeaveAppGuards;
+import com.anshuman.workflow.statemachine.state.LeaveAppState;
+import java.util.Set;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.statemachine.config.EnableStateMachineFactory;
+import org.springframework.statemachine.config.EnumStateMachineConfigurerAdapter;
+import org.springframework.statemachine.config.builders.StateMachineConfigurationConfigurer;
+import org.springframework.statemachine.config.builders.StateMachineStateConfigurer;
+import org.springframework.statemachine.config.builders.StateMachineTransitionConfigurer;
+
+@Configuration
+@EnableStateMachineFactory
+public class LeaveAppSMConfig extends EnumStateMachineConfigurerAdapter<LeaveAppState, LeaveAppEvent> {
+
+    @Override
+    public void configure(StateMachineConfigurationConfigurer<LeaveAppState, LeaveAppEvent> config) throws Exception {
+        config
+            .withConfiguration()
+                .machineId(LEAVE_APP_WF_V1)
+                .listener(new StateMachineListener<>())
+                .and()
+            .withMonitoring()
+                .monitor(new StateMachineMonitor<>())
+                .and();
+    }
+
+    @Override
+    public void configure(StateMachineStateConfigurer<LeaveAppState, LeaveAppEvent> states) throws Exception {
+        states
+            .withStates()
+                .initial(S_INITIAL)
+                .states(Set.of(S_CREATED, S_SUBMITTED, S_UNDER_PROCESS, S_CLOSED))
+                .end(S_COMPLETED);
+    }
+
+    @Override
+    public void configure(StateMachineTransitionConfigurer<LeaveAppState, LeaveAppEvent> transitions) throws Exception {
+
+        // initialize
+        transitions
+            .withExternal()
+                .name(TX_USER_CREATES_LEAVE_APP)
+                .source(S_INITIAL)
+                .event(E_INITIALIZE)
+                .target(S_CREATED)
+                .and();
+
+        // submit application
+        transitions
+            .withExternal()
+                .name(TX_USER_SUBMITS_LEAVE_APP)
+                .source(S_CREATED)
+                .event(E_SUBMIT)
+                .target(S_SUBMITTED)
+                .and();
+
+        // move application to be reviewed
+        transitions
+            .withExternal()
+                .name(TX_SYSTEM_TRIGGERS_LEAVE_APP_FOR_REVIEW)
+                .source(S_SUBMITTED)
+                .event(E_TRIGGER_REVIEW_OF)
+                .target(S_UNDER_PROCESS)
+                .and();
+
+        // cancel
+        transitions
+            .withExternal()
+                .name(TX_USER_CANCELS_CREATED_LEAVE_APP)
+                .source(S_CREATED)
+                .event(E_CANCEL)
+                .target(S_COMPLETED)
+                .action(TransitionActions::cancel)
+                .and()
+            .withExternal()
+                .name(TX_USER_CANCELS_LEAVE_APP_UNDER_REVIEW)
+                .source(S_UNDER_PROCESS)
+                .event(E_CANCEL)
+                .target(S_COMPLETED)
+                .action(TransitionActions::cancel)
+                .and();
+
+        // forward and approve
+        transitions
+            .withInternal()
+                .name(TX_REVIEWER_FORWARDS_APPLICATION)
+                .source(S_UNDER_PROCESS)
+                .event(E_FORWARD)
+                .guard(LeaveAppGuards::forward)
+                .action(TransitionActions::forward)
+                .and()
+            .withExternal()
+                .name(TX_REVIEWER_APPROVES_LEAVE_APP_IN_SERIAL_FLOW)
+                .source(S_UNDER_PROCESS)
+                .event(E_APPROVE)
+                .target(S_CLOSED)
+                .guard(LeaveAppGuards::approve)
+                .action(TransitionActions::approve)
+                .and();
+
+        // request changes
+        transitions
+            .withExternal()
+                .name(TX_REVIEWER_REQUESTS_CHANGES_FROM_USER_IN_SERIAL_FLOW)
+                .source(S_UNDER_PROCESS)
+                .event(E_REQUEST_CHANGES_IN)
+                .target(S_SUBMITTED)
+                .action(TransitionActions::requestChanges)
+                .and();
+
+        // reject
+        transitions
+            .withExternal()
+                .name(TX_REVIEWER_REJECTS_LEAVE_APP_IN_SERIAL_FLOW)
+                .source(S_UNDER_PROCESS)
+                .event(E_REJECT)
+                .target(S_CLOSED)
+                .action(TransitionActions::reject)
+                .and();
+
+        // roll back approval
+        transitions
+            .withExternal()
+                .name(TX_REVIEWER_ROLLS_BACK_APPROVAL_IN_SERIAL_FLOW)
+                .source(S_UNDER_PROCESS)
+                .event(E_ROLL_BACK)
+                .target(S_UNDER_PROCESS)
+                .guard(LeaveAppGuards::rollBackApproval)
+                .action(TransitionActions::rollBackApproval)
+                .and()
+            .withExternal()
+                .name(TX_REVIEWER_ROLLS_BACK_APPROVAL_IN_SERIAL_FLOW)
+                .source(S_CLOSED)
+                .event(E_ROLL_BACK)
+                .target(S_UNDER_PROCESS)
+                .guard(LeaveAppGuards::rollBackApproval)
+                .action(TransitionActions::rollBackApproval)
+                .and();
+
+        // complete
+        transitions
+            .withExternal()
+                .name(TX_SYSTEM_COMPLETES_LEAVE_APP)
+                .source(S_CLOSED)
+                .event(E_TRIGGER_COMPLETE)
+                .target(S_COMPLETED)
+                .and();
+
+    }
+}
