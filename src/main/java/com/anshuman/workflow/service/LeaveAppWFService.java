02af2f27d5a2fb9043b079adb47a9ef3e989b701
Anshuman Sanghvi <anshuman.sanghvi@gmail.com>
2023-03-02 16:20:35 +0530

    Rename leave app service

--- src/main/java/com/anshuman/workflow/service/LeaveAppWFService.java
+++ src/main/java/com/anshuman/workflow/service/LeaveAppWFService.java
@@ -17,7 +17,7 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 @Slf4j
 @Transactional(readOnly = true)
-public class LeaveApplicationWFService {
+public class LeaveAppWFService {
 
     private final LeaveAppStateMachineService leaveAppStateMachineService;
 
