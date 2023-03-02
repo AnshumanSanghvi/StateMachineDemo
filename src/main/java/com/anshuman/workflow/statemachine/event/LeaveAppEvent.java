51384ded3ebc1e70f858983f3564b9298a90d90c
Anshuman Sanghvi <anshuman.sanghvi@gmail.com>
2023-03-02 16:39:07 +0530

    LeaveAppEvent: add method to parse event by name

--- src/main/java/com/anshuman/workflow/statemachine/event/LeaveAppEvent.java
+++ src/main/java/com/anshuman/workflow/statemachine/event/LeaveAppEvent.java
@@ -18,8 +18,18 @@ public enum LeaveAppEvent {
     @Getter
     private final String humanReadableStatus;
 
+    private static final LeaveAppEvent[] values = LeaveAppEvent.values();
+
     LeaveAppEvent(String humanReadableStatus) {
         this.humanReadableStatus = humanReadableStatus;
+    }
+
+    public static LeaveAppEvent getByName(String name) {
+        for(LeaveAppEvent e : values) {
+            if(e.name().equalsIgnoreCase(name))
+                return e;
+        }
+        throw new IllegalArgumentException("No event with the given name found");
     }
 
 }
