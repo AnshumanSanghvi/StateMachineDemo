4b40d299400b14cccfdeb13b018412b679bfd653
Anshuman Sanghvi <anshuman.sanghvi@gmail.com>
2023-05-02 14:37:12 +0530

    Move request changes tests to nested class.

--- src/test/java/com/sttl/hrms/workflow/statemachine/builder/StateMachineSerialTest.java
+++ src/test/java/com/sttl/hrms/workflow/statemachine/builder/StateMachineSerialTest.java
@@ -7,10 +7,7 @@ import com.sttl.hrms.workflow.statemachine.SMConstants;
 import com.sttl.hrms.workflow.statemachine.builder.StateMachineBuilder.SMEvent;
 import com.sttl.hrms.workflow.statemachine.util.EventResultHelper;
 import com.sttl.hrms.workflow.statemachine.util.EventSendHelper;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.RepeatedTest;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.statemachine.ExtendedState;
@@ -100,139 +97,143 @@ class StateMachineSerialTest {
         assertTrue(stateMachine.isComplete());
     }
 
-    @Test
-    void requestChangesHappyPath() {
-        final ExtendedState extState = stateMachine.getExtendedState();
 
-        // create and submit application for review
-        List<PassEventDto> passEvents1 = createPassEvents(1L, LOAN_APPLICATION, 1L, 0, "", E_CREATE,
-                E_SUBMIT, E_TRIGGER_REVIEW_OF, E_TRIGGER_FLOW_JUNCTION);
-        EventSendHelper.passEvents(stateMachine, passEvents1);
-        assertEquals(S_SERIAL_APPROVAL_FLOW.name(), stateMachine.getState().getId());
+    @Nested
+    public class RequestChangesTest {
+        @Test
+        void requestChangesHappyPath() {
+            final ExtendedState extState = stateMachine.getExtendedState();
 
-        // reviewer 1 requests changes
-        List<PassEventDto> passEvents2 = createPassEvents(1L, LOAN_APPLICATION, 123L, 1, "please make changes",
-                E_REQUEST_CHANGES_IN);
-        EventSendHelper.passEvents(stateMachine, passEvents2);
-        assertEquals(0, extState.get(KEY_FORWARDED_COUNT, Integer.class));
-        assertEquals(S_CREATED.name(), stateMachine.getState().getId());
-        assertEquals(1, extState.get(KEY_RETURN_COUNT, Integer.class));
+            // create and submit application for review
+            List<PassEventDto> passEvents1 = createPassEvents(1L, LOAN_APPLICATION, 1L, 0, "", E_CREATE,
+                    E_SUBMIT, E_TRIGGER_REVIEW_OF, E_TRIGGER_FLOW_JUNCTION);
+            EventSendHelper.passEvents(stateMachine, passEvents1);
+            assertEquals(S_SERIAL_APPROVAL_FLOW.name(), stateMachine.getState().getId());
 
-        // user submits application for review again
-        passEvents1.removeIf(passEvent -> passEvent.getEvent().equalsIgnoreCase(E_CREATE.name()));
-        EventSendHelper.passEvents(stateMachine, passEvents1);
+            // reviewer 1 requests changes
+            List<PassEventDto> passEvents2 = createPassEvents(1L, LOAN_APPLICATION, 123L, 1, "please make changes",
+                    E_REQUEST_CHANGES_IN);
+            EventSendHelper.passEvents(stateMachine, passEvents2);
+            assertEquals(0, extState.get(KEY_FORWARDED_COUNT, Integer.class));
+            assertEquals(S_CREATED.name(), stateMachine.getState().getId());
+            assertEquals(1, extState.get(KEY_RETURN_COUNT, Integer.class));
 
-        // first reviewer forwards the application
-        List<PassEventDto> passEvents3 = createPassEvents(1L, LOAN_APPLICATION, 123L, 1, "forwarded",
-                E_FORWARD);
-        EventSendHelper.passEvents(stateMachine, passEvents3);
-        assertEquals(1, extState.get(KEY_FORWARDED_COUNT, Integer.class));
-        assertEquals(new Pair<>(1, 123L), extState.get(KEY_LAST_FORWARDED_BY, Pair.class));
+            // user submits application for review again
+            passEvents1.removeIf(passEvent -> passEvent.getEvent().equalsIgnoreCase(E_CREATE.name()));
+            EventSendHelper.passEvents(stateMachine, passEvents1);
 
-        // second reviewer forwards the application
-        List<PassEventDto> passEvents4 = createPassEvents(1L, LOAN_APPLICATION, 234L, 2, "forwarded",
-                E_FORWARD);
-        EventSendHelper.passEvents(stateMachine, passEvents4);
-        assertEquals(2, extState.get(SMConstants.KEY_FORWARDED_COUNT, Integer.class));
-        assertEquals(new Pair<>(2, 234L), extState.get(KEY_LAST_FORWARDED_BY, Pair.class));
+            // first reviewer forwards the application
+            List<PassEventDto> passEvents3 = createPassEvents(1L, LOAN_APPLICATION, 123L, 1, "forwarded",
+                    E_FORWARD);
+            EventSendHelper.passEvents(stateMachine, passEvents3);
+            assertEquals(1, extState.get(KEY_FORWARDED_COUNT, Integer.class));
+            assertEquals(new Pair<>(1, 123L), extState.get(KEY_LAST_FORWARDED_BY, Pair.class));
 
-        // third reviewer requests changes
-        List<PassEventDto> passEvents5 = createPassEvents(1L, LOAN_APPLICATION, 345L, 3, "please make changes",
-                E_REQUEST_CHANGES_IN);
-        EventSendHelper.passEvents(stateMachine, passEvents5);
-        assertEquals(0, extState.get(KEY_FORWARDED_COUNT, Integer.class));
-        assertEquals(S_CREATED.name(), stateMachine.getState().getId());
-        assertEquals(2, extState.get(KEY_RETURN_COUNT, Integer.class));
+            // second reviewer forwards the application
+            List<PassEventDto> passEvents4 = createPassEvents(1L, LOAN_APPLICATION, 234L, 2, "forwarded",
+                    E_FORWARD);
+            EventSendHelper.passEvents(stateMachine, passEvents4);
+            assertEquals(2, extState.get(SMConstants.KEY_FORWARDED_COUNT, Integer.class));
+            assertEquals(new Pair<>(2, 234L), extState.get(KEY_LAST_FORWARDED_BY, Pair.class));
 
-    }
+            // third reviewer requests changes
+            List<PassEventDto> passEvents5 = createPassEvents(1L, LOAN_APPLICATION, 345L, 3, "please make changes",
+                    E_REQUEST_CHANGES_IN);
+            EventSendHelper.passEvents(stateMachine, passEvents5);
+            assertEquals(0, extState.get(KEY_FORWARDED_COUNT, Integer.class));
+            assertEquals(S_CREATED.name(), stateMachine.getState().getId());
+            assertEquals(2, extState.get(KEY_RETURN_COUNT, Integer.class));
 
-    @Test
-    void requestChangesLimit() {
-        final ExtendedState extState = stateMachine.getExtendedState();
+        }
 
-        // create and submit application for review
-        List<PassEventDto> passEvents1 = createPassEvents(1L, LOAN_APPLICATION, 1L, 0, "", E_CREATE,
-                E_SUBMIT, E_TRIGGER_REVIEW_OF, E_TRIGGER_FLOW_JUNCTION);
-        EventSendHelper.passEvents(stateMachine, passEvents1);
-        assertEquals(S_SERIAL_APPROVAL_FLOW.name(), stateMachine.getState().getId());
+        @Test
+        void requestChangesLimit() {
+            final ExtendedState extState = stateMachine.getExtendedState();
 
-        // reviewer 1 requests changes
-        List<PassEventDto> passEvents2 = createPassEvents(1L, LOAN_APPLICATION, 123L, 1, "please make changes",
-                E_REQUEST_CHANGES_IN);
-        EventSendHelper.passEvents(stateMachine, passEvents2);
-        assertEquals(0, extState.get(KEY_FORWARDED_COUNT, Integer.class));
-        assertEquals(S_CREATED.name(), stateMachine.getState().getId());
-        assertEquals(1, extState.get(KEY_RETURN_COUNT, Integer.class));
+            // create and submit application for review
+            List<PassEventDto> passEvents1 = createPassEvents(1L, LOAN_APPLICATION, 1L, 0, "", E_CREATE,
+                    E_SUBMIT, E_TRIGGER_REVIEW_OF, E_TRIGGER_FLOW_JUNCTION);
+            EventSendHelper.passEvents(stateMachine, passEvents1);
+            assertEquals(S_SERIAL_APPROVAL_FLOW.name(), stateMachine.getState().getId());
 
-        // user submits application for review again
-        passEvents1.removeIf(passEvent -> passEvent.getEvent().equalsIgnoreCase(E_CREATE.name()));
-        EventSendHelper.passEvents(stateMachine, passEvents1);
+            // reviewer 1 requests changes
+            List<PassEventDto> passEvents2 = createPassEvents(1L, LOAN_APPLICATION, 123L, 1, "please make changes",
+                    E_REQUEST_CHANGES_IN);
+            EventSendHelper.passEvents(stateMachine, passEvents2);
+            assertEquals(0, extState.get(KEY_FORWARDED_COUNT, Integer.class));
+            assertEquals(S_CREATED.name(), stateMachine.getState().getId());
+            assertEquals(1, extState.get(KEY_RETURN_COUNT, Integer.class));
 
-        // reviewer 1 requests changes 2nd time
-        EventSendHelper.passEvents(stateMachine, passEvents2);
-        assertEquals(0, extState.get(KEY_FORWARDED_COUNT, Integer.class));
-        assertEquals(S_CREATED.name(), stateMachine.getState().getId());
-        assertEquals(2, extState.get(KEY_RETURN_COUNT, Integer.class));
+            // user submits application for review again
+            passEvents1.removeIf(passEvent -> passEvent.getEvent().equalsIgnoreCase(E_CREATE.name()));
+            EventSendHelper.passEvents(stateMachine, passEvents1);
 
-        // user submits application for review again
-        EventSendHelper.passEvents(stateMachine, passEvents1);
+            // reviewer 1 requests changes 2nd time
+            EventSendHelper.passEvents(stateMachine, passEvents2);
+            assertEquals(0, extState.get(KEY_FORWARDED_COUNT, Integer.class));
+            assertEquals(S_CREATED.name(), stateMachine.getState().getId());
+            assertEquals(2, extState.get(KEY_RETURN_COUNT, Integer.class));
 
-        // reviewer 1 requests changes 3rd time
-        EventSendHelper.passEvents(stateMachine, passEvents2);
-        assertEquals(0, extState.get(KEY_FORWARDED_COUNT, Integer.class));
-        assertEquals(S_CREATED.name(), stateMachine.getState().getId());
-        assertEquals(3, extState.get(KEY_RETURN_COUNT, Integer.class));
+            // user submits application for review again
+            EventSendHelper.passEvents(stateMachine, passEvents1);
 
-        // user submits application for review again
-        EventSendHelper.passEvents(stateMachine, passEvents1);
+            // reviewer 1 requests changes 3rd time
+            EventSendHelper.passEvents(stateMachine, passEvents2);
+            assertEquals(0, extState.get(KEY_FORWARDED_COUNT, Integer.class));
+            assertEquals(S_CREATED.name(), stateMachine.getState().getId());
+            assertEquals(3, extState.get(KEY_RETURN_COUNT, Integer.class));
 
-        // reviewer 1 and reviewer 2 forwards the application
-        List<PassEventDto> passEvents3 = createPassEvents(1L, LEAVE_APPLICATION, 123L, 1, "forwarded",
-                E_FORWARD);
-        EventSendHelper.passEvents(stateMachine, passEvents3);
-        List<PassEventDto> passEvents4 = createPassEvents(1L, LOAN_APPLICATION, 234L, 2, "forwarded",
-                E_FORWARD);
-        EventSendHelper.passEvents(stateMachine, passEvents4);
+            // user submits application for review again
+            EventSendHelper.passEvents(stateMachine, passEvents1);
 
-        // reviewer 3 requests changes (total, 4th time)
-        List<PassEventDto> passEvents5 = createPassEvents(1L, LOAN_APPLICATION, 345L, 3, "please make changes",
-                E_REQUEST_CHANGES_IN);
-        EventSendHelper.passEvents(stateMachine, passEvents5);
-        assertEquals(3, extState.get(KEY_RETURN_COUNT, Integer.class));
-        assertEquals(2, extState.get(KEY_FORWARDED_COUNT, Integer.class));
-        assertEquals(S_SERIAL_APPROVAL_FLOW.name(), stateMachine.getState().getId());
-    }
+            // reviewer 1 and reviewer 2 forwards the application
+            List<PassEventDto> passEvents3 = createPassEvents(1L, LEAVE_APPLICATION, 123L, 1, "forwarded",
+                    E_FORWARD);
+            EventSendHelper.passEvents(stateMachine, passEvents3);
+            List<PassEventDto> passEvents4 = createPassEvents(1L, LOAN_APPLICATION, 234L, 2, "forwarded",
+                    E_FORWARD);
+            EventSendHelper.passEvents(stateMachine, passEvents4);
 
-    @Test
-    void requestChangesOrder() {
-        final ExtendedState extState = stateMachine.getExtendedState();
+            // reviewer 3 requests changes (total, 4th time)
+            List<PassEventDto> passEvents5 = createPassEvents(1L, LOAN_APPLICATION, 345L, 3, "please make changes",
+                    E_REQUEST_CHANGES_IN);
+            EventSendHelper.passEvents(stateMachine, passEvents5);
+            assertEquals(3, extState.get(KEY_RETURN_COUNT, Integer.class));
+            assertEquals(2, extState.get(KEY_FORWARDED_COUNT, Integer.class));
+            assertEquals(S_SERIAL_APPROVAL_FLOW.name(), stateMachine.getState().getId());
+        }
 
-        // create and submit application for review
-        List<PassEventDto> passEvents1 = createPassEvents(1L, LOAN_APPLICATION, 1L, 0, "", E_CREATE,
-                E_SUBMIT, E_TRIGGER_REVIEW_OF, E_TRIGGER_FLOW_JUNCTION);
-        EventSendHelper.passEvents(stateMachine, passEvents1);
-        assertEquals(S_SERIAL_APPROVAL_FLOW.name(), stateMachine.getState().getId());
+        @Test
+        void requestChangesOrder() {
+            final ExtendedState extState = stateMachine.getExtendedState();
 
-        // test that if reviewer 2 requests changes then they won't be accepted as its out of reviewer order
-        List<PassEventDto> passEvents2 = createPassEvents(1L, LOAN_APPLICATION, 234L, 2, "please make changes",
-                E_REQUEST_CHANGES_IN);
-        EventSendHelper.passEvents(stateMachine, passEvents2);
-        assertEquals(0, extState.get(KEY_FORWARDED_COUNT, Integer.class));
-        assertEquals(S_SERIAL_APPROVAL_FLOW.name(), stateMachine.getState().getId());
-        assertEquals(0, extState.get(KEY_RETURN_COUNT, Integer.class));
+            // create and submit application for review
+            List<PassEventDto> passEvents1 = createPassEvents(1L, LOAN_APPLICATION, 1L, 0, "", E_CREATE,
+                    E_SUBMIT, E_TRIGGER_REVIEW_OF, E_TRIGGER_FLOW_JUNCTION);
+            EventSendHelper.passEvents(stateMachine, passEvents1);
+            assertEquals(S_SERIAL_APPROVAL_FLOW.name(), stateMachine.getState().getId());
 
-        // first reviewer forwards the application
-        List<PassEventDto> passEvents3 = createPassEvents(1L, LOAN_APPLICATION, 123L, 1, "forwarded",
-                E_FORWARD);
-        EventSendHelper.passEvents(stateMachine, passEvents3);
-        assertEquals(1, extState.get(KEY_FORWARDED_COUNT, Integer.class));
-        assertEquals(new Pair<>(1, 123L), extState.get(KEY_LAST_FORWARDED_BY, Pair.class));
+            // test that if reviewer 2 requests changes then they won't be accepted as its out of reviewer order
+            List<PassEventDto> passEvents2 = createPassEvents(1L, LOAN_APPLICATION, 234L, 2, "please make changes",
+                    E_REQUEST_CHANGES_IN);
+            EventSendHelper.passEvents(stateMachine, passEvents2);
+            assertEquals(0, extState.get(KEY_FORWARDED_COUNT, Integer.class));
+            assertEquals(S_SERIAL_APPROVAL_FLOW.name(), stateMachine.getState().getId());
+            assertEquals(0, extState.get(KEY_RETURN_COUNT, Integer.class));
 
-        // test that now if reviewer 2 requests changes then they will be accepted
-        EventSendHelper.passEvents(stateMachine, passEvents2);
-        assertEquals(0, extState.get(KEY_FORWARDED_COUNT, Integer.class));
-        assertEquals(S_CREATED.name(), stateMachine.getState().getId());
-        assertEquals(1, extState.get(KEY_RETURN_COUNT, Integer.class));
+            // first reviewer forwards the application
+            List<PassEventDto> passEvents3 = createPassEvents(1L, LOAN_APPLICATION, 123L, 1, "forwarded",
+                    E_FORWARD);
+            EventSendHelper.passEvents(stateMachine, passEvents3);
+            assertEquals(1, extState.get(KEY_FORWARDED_COUNT, Integer.class));
+            assertEquals(new Pair<>(1, 123L), extState.get(KEY_LAST_FORWARDED_BY, Pair.class));
+
+            // test that now if reviewer 2 requests changes then they will be accepted
+            EventSendHelper.passEvents(stateMachine, passEvents2);
+            assertEquals(0, extState.get(KEY_FORWARDED_COUNT, Integer.class));
+            assertEquals(S_CREATED.name(), stateMachine.getState().getId());
+            assertEquals(1, extState.get(KEY_RETURN_COUNT, Integer.class));
+        }
     }
 
 
