2b2306c2db77fee343b8e45d252c48f2dd7cc41a
Anshuman Sanghvi <anshuman.sanghvi@gmail.com>
2023-06-12 16:23:30 +0530

    Create Admin Tests

--- src/test/java/com/sttl/hrms/workflow/statemachine/builder/StateMachineAdminTest.java
+++ src/test/java/com/sttl/hrms/workflow/statemachine/builder/StateMachineAdminTest.java
@@ -1,5 +1,6 @@
 package com.sttl.hrms.workflow.statemachine.builder;
 
+import com.sttl.hrms.workflow.data.Pair;
 import com.sttl.hrms.workflow.data.enums.WorkflowType;
 import com.sttl.hrms.workflow.data.model.entity.WorkflowTypeEntity;
 import com.sttl.hrms.workflow.resource.dto.PassEventDto;
@@ -15,17 +16,18 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import static com.sttl.hrms.workflow.data.enums.WorkflowType.LOAN_APPLICATION;
 import static com.sttl.hrms.workflow.statemachine.SMConstants.*;
-import static com.sttl.hrms.workflow.statemachine.builder.StateMachineBuilder.SMEvent.*;
+import static com.sttl.hrms.workflow.statemachine.builder.StateMachineBuilder.SMEvent.E_CREATE;
 import static com.sttl.hrms.workflow.statemachine.builder.StateMachineBuilder.SMState.S_CLOSED;
-import static com.sttl.hrms.workflow.statemachine.builder.StateMachineBuilder.SMState.S_SERIAL_APPROVAL_FLOW;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class StateMachineAdminTest {
     private final StateMachine<String, String> stateMachine;
+    private static final Long applicant = 10L;
     private static final Long reviewer1 = 123L;
     private static final Long reviewer2 = 234L;
     private static final Long reviewer3 = 345L;
@@ -35,7 +37,8 @@ public class StateMachineAdminTest {
 
     public StateMachineAdminTest() throws Exception {
         String stateMachineName = "testStateMachine";
-        Map<Integer, Long> reviewerMap = Map.of(1, reviewer1, 2, reviewer2, 3, reviewer3);
+        Map<Integer, Set<Long>> reviewerMap = Map.of(1, Set.of(reviewer1), 2, Set.of(reviewer2), 3,
+                Set.of(reviewer3));
         WorkflowTypeEntity.WorkflowProperties wfProps = new WorkflowTypeEntity.WorkflowProperties();
         wfProps.setAdminRoleIds(List.of(admin1, admin2, admin3));
         wfProps.setRollBackApproval(true);
@@ -43,7 +46,7 @@ public class StateMachineAdminTest {
         wfProps.setParallelApproval(false);
         wfProps.setRollbackMaxCount(3);
         wfProps.setChangeReqMaxCount(3);
-        this.stateMachine = StateMachineBuilder.createStateMachine(stateMachineName, reviewerMap, wfProps);
+        this.stateMachine = StateMachineBuilder.createStateMachine(stateMachineName, reviewerMap, wfProps, applicant);
     }
 
     @BeforeEach
@@ -61,7 +64,7 @@ public class StateMachineAdminTest {
         Long wfInstanceId = 0L;
         // create and submit an application for review
         createApplication(wfInstanceId);
-        assertEquals(S_SERIAL_APPROVAL_FLOW.name(), stateMachine.getState().getId());
+        assertEquals(S_CLOSED.name(), stateMachine.getState().getId());
     }
 
     @Test
@@ -70,10 +73,9 @@ public class StateMachineAdminTest {
         ExtendedState extState = stateMachine.getExtendedState();
 
         // create and submit an application for review
-        List<PassEventDto> passEvents = createPassEvents(wfInstanceId, LOAN_APPLICATION, admin3, 0, "", E_CREATE);
-        EventSendHelper.passEvents(stateMachine, passEvents);
+        createApplication(wfInstanceId);
 
-        assertEquals(extState.get(KEY_APPROVE_BY,Long.class), admin3);
+        assertEquals(extState.get(KEY_APPROVE_BY, Pair.class).getSecond(), admin3);
         assertNotNull(extState.get(KEY_APPROVE_COMMENT, String.class));
         assertEquals(S_CLOSED.name(), stateMachine.getState().getId());
         assertEquals(VAL_APPROVED,extState.get(KEY_CLOSED_STATE_TYPE, String.class));
@@ -83,7 +85,7 @@ public class StateMachineAdminTest {
 
     private List<PassEventDto> createApplication(final Long wfInstanceId) {
         List<PassEventDto> passEvents = createPassEvents(wfInstanceId, LOAN_APPLICATION, admin3, 0, "create " +
-                "application", E_CREATE, E_SUBMIT, E_TRIGGER_REVIEW_OF, E_TRIGGER_FLOW_JUNCTION);
+                "application", E_CREATE);
         EventSendHelper.passEvents(stateMachine, passEvents);
         return passEvents;
     }
